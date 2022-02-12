96fac2d7b8e33d11c4ec5ad8aa315e018d24ad5d
Logan Woolf <loganwoolf@gmail.com>
2022-02-12 22:09:55 +0000

    Create task router and root level routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cookieParser = require('cookie-parser');
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -18,7 +19,7 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
-
+app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
@@ -33,29 +34,16 @@ app.use(
 
 app.use(express.static("public"));
 
-// Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
-const widgetsPageRoutes = require("./routes/widgetsPage");
+// Define and enable routers
+const taskRoutes = require("./routes/tasks.js");
+app.use("/tasks", taskRoutes(db));
 
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
-
-app.use("/widgets", widgetsPageRoutes(db));
-
-// Note: mount other resources here, using the same pattern above
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-
-app.get("/", (req, res) => {
-  res.render("index");
+// Define root level routes
+app.get('/login', (req, res) => {
+  res.cookie('userID', 1); // hard coded to log user 1 in on request
+  res.redirect('/'); // redirects to static "public"
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
