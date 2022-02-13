6149203c399688a646fd914b84f34ec8e2aaf5a8
adrianetodesign <adrianetodesign@gmail.com>
2022-02-13 21:07:24 +0000

    Add escape and createTaskElement functions

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,25 +1,53 @@
 // Client facing scripts here
 $(() => {
-  console.log("ready");
-  $("#users").on('click', onClick);
-  $("#clear").on('click', onClear);
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const createTaskElement = function(task) {
+    let $htmlTask = `
+    <article class="task">
+    <div>
+      <p>${escape(task.category_id)}</p>
+    </div>
+    <div>
+      <p>${escape(task.body)}</p>
+    </div>
+    <div>
+      <p>${escape(task.time_added)}</p>
+    </div>
+    <div>
+      <input type="checkbox">
+    </div>
+    <div class="kebab">
+      <i class="fa-solid fa-ellipsis-vertical fa-xl"></i>
+    </div>
+    </article>
+    `;
+    return $htmlTask;
+  }
+
+  //--- Given an array of tasks, renders them in their proper container on the page.
+  const renderTasks = function(tasks) {
+    // Prepend to ensure most recent task is placed on top.
+    for (const task of tasks) {
+      $("#tasks-container").prepend(createTaskElement(task));
+    }
+  };
+
+  const loadTasks = function() {
+    $.get("/tasks")
+      .then((data) => {
+        $("#tasks-container").empty();
+        console.log(data.tasks);
+        renderTasks(data.tasks);
+      }).catch((err) => {
+        console.log("An error has occured:", err);
+      });
+  };
+  loadTasks();
+
 });
 
-const onClear = function() {
-  const list = $("#list");
-  list.empty();
-};
-
-const onClick = function() {
-  $.get("/api/users")
-    .then((data) => {
-      const list = $("#list");
-
-      for (user of data.users) {
-        const li = `<li>${user.name}</li>`;
-        list.append(li);
-      }
-
-    });
-
-};
