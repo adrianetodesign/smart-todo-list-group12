ca3d64cc4ff227349f64ceef523eae5d4bc58e3f
adrianetodesign <adrianetodesign@gmail.com>
2022-02-13 21:29:52 +0000

    Create loadUser function

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -48,5 +48,17 @@ $(() => {
     };
     loadTasks();
 
+    const loadUser = function() {
+      $.get("/users")
+        .then((data) => {
+          console.log(data);
+          $("#user-name").empty();
+          $("#user-name").html(`${data.users[0].name}'s`);
+        }).catch((err) => {
+          console.log("An error has occured:", err);
+        });
+    };
+    loadUser();
+
 });
 
