d9b794876ecf732bc9af68462dca1b07f9b71b33
Logan Woolf <loganwoolf@gmail.com>
2022-02-13 00:02:36 +0000

    Add route for [un]marking a task as done
    Route returns the new version of the entry
    Added checks for whether a task is owned for all routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -10,8 +10,45 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
+  router.post("/:id/done", (req,res) => {
+    // Toggles the is_completed boolean on the entry and returns
+    // the updated version
+    const userID = req.cookies.userID;
+    const taskID = req.params.id;
+
+    if (!userID) {
+      res.redirect('/login');
+      return;
+    }
+
+    const queryString = `
+      UPDATE tasks
+      SET is_completed = NOT is_completed
+      WHERE id = $1
+      AND user_id = $2
+      RETURNING *`;
+
+    const values = [taskID, userID];
+
+    db.query(queryString, values)
+      .then(data => {
+        const task = data.rows[0];
+        res.json({ task });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message});
+      });
+
+  });
+
   router.get("/:id", (req, res) => {
-    const cookie = req.cookies.userID;
+    const userID = req.cookies.userID;
+    if (!userID) {
+      res.redirect('/login');
+      return;
+    }
 
     const queryString = `
       SELECT * FROM tasks
@@ -19,7 +56,7 @@ module.exports = (db) => {
       AND id = $2
       `;
 
-    const values = [cookie, req.params.id];
+    const values = [userID, req.params.id];
 
     db.query(queryString, values)
       .then(data => {
@@ -34,14 +71,18 @@ module.exports = (db) => {
   });
 
   router.get("/", (req, res) => {
-    const cookie = req.cookies.userID;
+    const userID = req.cookies.userID;
+    if (!userID) {
+      res.redirect('/login');
+      return;
+    }
 
     const queryString = `
       SELECT * FROM tasks
       WHERE user_id = $1
       `;
 
-    const values = [cookie];
+    const values = [userID];
     console.log(values);
     db.query(queryString, values)
       .then(data => {
