a96cfdf09dca3fec2c20edd5efd1b2e36408fec6
adrianetodesign <adrianetodesign@gmail.com>
2022-02-15 23:24:05 +0000

    Remove unnecessary files

--- public/scripts/utility.js
+++
@@ -1,5 +0,0 @@
-
-const formatDate = function(stringDate) {
-  // return new Date(stringDate).toLocaleString('en-GB', {year: "numeric", month: "long", day: "numeric", hour: 'numeric', minute: "numeric" });
-  return format(new Date(stringDate));
-}
