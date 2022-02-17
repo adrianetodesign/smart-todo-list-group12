47c569ea455608ad6343c077e5bee22c919ceef5
adrianetodesign <adrianetodesign@gmail.com>
2022-02-17 18:35:41 +0000

    Remove unnecessary comments and old code

--- routes/classify.js
+++ routes/classify.js
@@ -1,9 +1,7 @@
 const request = require('request-promise-native');
 
 const classify = function(searchTerm) {
-  // return requestWolframAlpha(searchTerm);
   return requestGoogle(searchTerm);
-  // return requestYelp(searchTerm);
 };
 
 const requestYelp = function(searchTerm) {
@@ -17,7 +15,6 @@ const requestYelp = function(searchTerm) {
       term: searchTerm,
       location: 'Vancouver',
       categories: 'restaurants',
-      // eslint-disable-next-line camelcase
       sort_by: 'best_match',
       limit: 3
     }
@@ -35,27 +32,6 @@ const requestYelp = function(searchTerm) {
     return { classNumber: 4, className: 'products'};
   });
 };
-
-/* *
-* const requestWolframAlpha = function(searchTerm) {
-*  //  'https://api.wolframalpha.com/v2/query?appid=TY79PP-25XWKY68LE&input=the%20hobbit&output=json'
-*  const options = {
-*    uri: 'http://api.wolframalpha.com/v2/query?',
-*    qs: {
-*      appid: process.env.WOLFRAM_ALPHA,
-*      input: searchTerm,
-*      format: 'plaintext',
-*      output: 'json'
-*    }
-*  };
-*
-*  return request(options).then(data => {
-*    const queryResult = JSON.parse(data).queryresult.datatypes;
-*    const classes = queryResult.split(',');
-*    return classes;
-*  });
-* };
-* */
 
 const requestGoogle = function(searchTerm) {
   const options = {
